2d14c94ce88368eb4e27dd767f35b666496533ca
Vinicius Henrique <60949952+viniciushenriqueteixeira@users.noreply.github.com>
2020-11-23 12:47:59 -0300

    melhorando js

---
+++ battleship.js
@@ -0,0 +1,27 @@
+var view = {
+displayMessage: function(msg){
+
+    var messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
+
+    },
+    displayHit:function(location){
+        var cell = document.getElementById(location);
+        cell.setAttribute("class","hit");
+
+    },
+    displayMiss:function(location){
+        var cell = document.getElementById(location);
+        cell.setAttribute("class","miss");
+
+    }
+
+};
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+
+view.displayMessage("bora vamos la se decepicionar");
